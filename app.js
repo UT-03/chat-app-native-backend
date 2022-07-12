43322efb2954555e3dfbc4d3aa141cf09b3bf9ac
Utkarsh Tiwari <79445012+UT-03@users.noreply.github.com>
2022-07-12 10:39:12 +0530

    inital commit

---
+++ app.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const AuthRoutes = require('./routes/Auth');
+
+// Initializing express app
+const app = express();
+
+// Handling cors errors
+app.use(cors());
+
+// Extracting json data from requests
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Routes here
+app.use('/api/auth', AuthRoutes);
+
+// Handling error
+app.use((error, req, res, next) => {
+    if (res.headerSent) {
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({ message: error.message || 'Something went wrong, please try again later.' });
+});
+
+// Connecting to database
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => {
+        // Starting server
+        return app.listen(process.env.PORT);
+    })
+    .then(server => {
+        // Adding socket.io
+        // addSockets(server);
+
+        // consoling result
+        console.log('Connected to database and server started at port ', process.env.PORT);
+    })
+    .catch(err => {
+        console.log(err);
+    });
